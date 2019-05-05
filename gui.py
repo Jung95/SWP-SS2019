f654bc4ad1c27200a898362746c4bfa47da6d3fa
Jung95 <jung95219@gmail.com>
2019-05-05 20:13:36 +0200

    Graphical User Interface

---
+++ gui.py
@@ -0,0 +1,76 @@
+import crawler
+from tkinter import *
+from tkinter import messagebox
+import tkinter as tk
+import json #import json module
+
+root = Tk()
+root.title('Bundesliga Vorhersage')
+
+league_result = []
+team_list=["A List wasn't loaded"]
+def but1onClick():
+    result = json.loads(crawler.crawling())# Crawler loads Data
+    team_list = result['TeamList'] # Save the TeamList in List
+    league_result = result['LeagueResult'] # Save the LeagueResult in List
+    season.configure(text= result['year']) #change the Season in the Text
+    # reset var and delete all old options
+    tkvar1.set(team_list[0]) #
+    tkvar2.set(team_list[1])
+    popupMenu1['menu'].delete(0, 'end')
+    popupMenu2['menu'].delete(0, 'end')
+    #reload all teams
+    for team in team_list:
+        popupMenu1['menu'].add_command(label=team, command=tk._setit(tkvar1, team))
+        popupMenu2['menu'].add_command(label=team, command=tk._setit(tkvar2, team))
+        
+
+def traning():
+    return 0
+
+# Set Labels
+season = Label(root, text="Season : it wasn't loaded")
+season.grid(row=1, column=1)
+select_Team = Label(root, text="Choose two Teams")
+select_Team.grid(row = 3, column = 1)
+ 
+#set buttons
+btn1 = Button(root, text="Crawling", command=but1onClick) # crawl
+btn1.grid(row=0, column=1)
+btn2 = Button(root, text="Training Start", command=traning)
+btn2.grid(row=2, column=1)
+vstext = Label(root, text="VS")
+vstext.grid(row=4, column=1)
+result_text = Label(root, text="Expected results")
+result_text.grid(row=5, column=1)
+result_team1 = Label(root, text="it wasn't trained")
+result_team1.grid(row=5, column=0)
+result_team2 = Label(root, text="it wasn't trained")
+result_team2.grid(row=5, column=2)
+
+# Create a Tkinter variable
+tkvar1 = StringVar(root)
+tkvar2 = StringVar(root)
+
+# Dictionary with options
+tkvar1.set("A List wasn't loaded") # set the default option
+tkvar2.set("A List wasn't loaded") # set the default option
+
+popupMenu1 = OptionMenu(root, tkvar1, *team_list)
+popupMenu2 = OptionMenu(root, tkvar2, *team_list)
+popupMenu1.grid(row = 4, column =0)
+popupMenu2.grid(row = 4, column =2)
+
+# on change dropdown value
+
+def change_dropdown1(*args):
+    print( tkvar1.get() )
+def change_dropdown2(*args):
+    print( tkvar2.get() )
+
+# link function to change dropdown
+tkvar1.trace('w', change_dropdown1)
+tkvar2.trace('w', change_dropdown2)
+
+root.mainloop()
+
