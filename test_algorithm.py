ffc3be486765c43d74f9da53d7bd5275b805b851
Michelle Schlicher <michelle-schlicher@t-online.de>
2019-07-01 14:33:13 +0200

    test for miniAlgo.initHisto

--- test_algorithm.py
+++ test_algorithm.py
@@ -1,4 +1,5 @@
 import pytest
+import os
 import miniAlgo
 
 minialgo = miniAlgo.Algorithmus()
@@ -16,6 +17,8 @@ def test_init():
     assert minialgo.maxGoal==0, "maxGoal initialised wrong"
     
 def test_setDate():
+    """Test for right setting of variables
+    """
     minialgo.setDate(2018,5,2018,6,2019)
     assert minialgo.startYear==2018, "startYear set wrong"
     assert minialgo.startDay==5, "startDay set wrong"
@@ -23,7 +26,16 @@ def test_setDate():
     assert minialgo.endDay==6, "endDay set wrong"
     assert minialgo.nowYear==2019, "nowYear set wrong"
 
-
-    
+def test_setHisto():
+    """Test for right histogram initialisation
+    """
+    minialgo.setDate(2018,5,2018,6,2019)
+    minialgo.setHisto()
+    assert os.path.isfile("5_2018_6_2018.csv"), "there is no CSV file"
+    assert minialgo.maxGoal==2, "maxGoal not detected correctly"
+    # test falls daten falsch herum 
+    # test für histogram befüllung
+    minialgo.setDate(2018,6,2018,5,2019)
+    assert os.path.isfile("6_2018_5_2018.csv"), "dates are invalid"
 
 
