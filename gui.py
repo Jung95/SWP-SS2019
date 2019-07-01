f94a81324dc2f524c454ded45d69c8c067ff8843
Jung95 <jung95219@gmail.com>
2019-07-01 12:42:18 +0200

    Made Oder of Selections in GUI and Status

--- gui.py
+++ gui.py
@@ -19,26 +19,67 @@ else:
     league_year = year
 isTrained = False
 minialgo = miniAlgo.Algorithmus()
+statusIndc = 0
 
 # Option Lists
-year_list = [str(league_year)]
+start_Year_list = [str(league_year)]
 for year in range(1,10):
-    year_list.append(str(league_year-year))
-day_list = [str(1)]
+    start_Year_list.append(str(league_year-year))
+start_Match_list = [str(1)]
 for day in range(2,35):
-    day_list.append(str(day))
+    start_Match_list.append(str(day))
 team_list=["No list loaded"]
+end_Year_list = [str(league_year)]
+end_Match_list = [str(34)]
 
 # GUI start
 root = Tk()
 root.title('Bundesliga Prediction')
 
 def makeCrawlThread():
-    season.configure(text= "Loading, please wait") #change the Season in the Text
+    NextOrResetBtn.config(state="disabled")
+    crawlBtn.config(state="disabled")
+    endMatchMenu.config(state="disabled")
+    endYearMenu.config(state="disabled")
+    status.configure(text= "Crawling, please wait") #change the Season in the Text
     t1 = threading.Thread(target=crawling) #make Thread
     t1.daemon = True  # make as Daemon Thread
     t1.start()  # start
-
+def NextOrReset():
+    NextOrResetBtn.config(text="Reset")
+    global end_Year_list
+    global end_Match_list
+    global statusIndc
+    global league_year
+    global year
+    global startYear
+    global endYearMenu
+    if statusIndc==0:  # if Next
+        NextOrResetBtn.configure(text="Reset")
+        endYearMenu.config(state="normal")
+        endMatchMenu.config(state="normal")
+        startYearMenu.config(state="disabled")
+        startMatchMenu.config(state="disabled")
+        crawlBtn.config(state="normal")
+        status.configure(text= "Set a end date, and Crawl")
+        # reset var and delete all old options
+        for x in range(1,league_year-int(startYear.get())+1):
+            endYearMenu['menu'].add_command(label=str(league_year-x), command=tkinter1._setit(endYear, str(league_year-x)))
+        endYear.set(str(league_year))
+        statusIndc=1
+    elif statusIndc==1:  # if Reset
+        NextOrResetBtn.configure(text="Next")
+        endYearMenu.config(state="disabled")
+        endMatchMenu.config(state="disabled")
+        startYearMenu.config(state="normal")
+        startMatchMenu.config(state="normal")
+        crawlBtn.config(state="disabled")
+        status.configure(text= "Set a start date")
+        endYearMenu['menu'].delete(0, 'end')
+        endMatchMenu['menu'].delete(0, 'end')
+        endYearMenu['menu'].add_command(label=str(league_year), command=tkinter1._setit(str(league_year), str(league_year))) 
+        endMatchMenu['menu'].add_command(label=str(34), command=tkinter1._setit(str(34), str(34)))
+        statusIndc=0
 def TeamMenuChange(home, away):
     if not isTrained:
         return
@@ -88,7 +129,7 @@ def crawling():
     tester.crawlTest(int(startYear.get()),int(startMatch.get()),int(endYear.get()),int(endMatch.get()))
     
     team_list = crawler.get_team_list(league_year) # Save the TeamList in List
-    season.configure(text= 'Bundesliga '+ str(league_year) + '/' + str(league_year+1)) #change the Season in the Text
+
     # reset var and delete all old options
     hometeamVar.set(team_list[0])
     guestteamVar.set(team_list[1])
@@ -99,11 +140,13 @@ def crawling():
         homeTeamMenu['menu'].add_command(label=team, command=tkinter1._setit(hometeamVar, team))
         geustTeamMenu['menu'].add_command(label=team, command=tkinter1._setit(guestteamVar, team))
     startTrainBtn.config(state="normal")
-    crwalBtn.config(state="disabled")
+    algoSelMenu.config(state="normal")
+    crawlBtn.config(state="disabled")
     startYearMenu.config(state="disabled")
     endYearMenu.config(state="disabled")
     startMatchMenu.config(state="disabled")
     endMatchMenu.config(state="disabled")
+    status.configure(text= "Select Algo and Traning") 
        
 def traning():
     startTrainBtn.config(state="disabled")
@@ -135,47 +178,54 @@ def traning():
     # table for tomorrows machtes with winning chances
     if not wasEnded:
         nxtMtchs = Label(root, text='Next Matches')
-        nxtMtchs.grid(row=7, column = 1)
+        nxtMtchs.grid(row=0, column = 4)
         matchList = [0]*45
         for match in range(9):
             result = minialgo.predict(listHome[match], listGuest[match])
-            Label(root, relief=RIDGE, text=listHome[match]).grid(row=8+match*2, column=0)
-            Label(root,relief=RIDGE, text=listGuest[match]).grid(row=8+match*2, column=2)
-            Label(root, text=str(result[0])+"%").grid(row=9+match*2, column=0)
-            Label(root, text=str(result[1])+"%").grid(row=9+match*2, column=1)
-            Label(root, text=str(result[2])+"%").grid(row=9+match*2, column=2)
-            Label(root, text='vs.').grid(row=8 + 2*match,column=1)
+            Label(root, relief=RIDGE, text=listHome[match]).grid(row=1+match*2, column=3)
+            Label(root,relief=RIDGE, text=listGuest[match]).grid(row=1+match*2, column=5)
+            Label(root, text=str(result[0])+"%").grid(row=2+match*2, column=3)
+            Label(root, text=str(result[1])+"%").grid(row=2+match*2, column=4)
+            Label(root, text=str(result[2])+"%").grid(row=2+match*2, column=5)
+            Label(root, text='vs.').grid(row=1 + 2*match,column=4)
     else: 
-        Label(root, text='season has ended').grid(row= 9, column=1)
+        Label(root, text='season has ended').grid(row=2, column=4)
     
 # Set Labels
-season = Label(root, text="Season : not loaded")
-season.grid(row=1, column=1)
-start_season = Label(root, text="Start Match")
-start_season.grid(row=2, column=0)
-end_season = Label(root, text="End Match")
-end_season.grid(row=2, column=2)
+status = Label(root, text="Set a Start Date")
+status.grid(row=0, column=1)
+start_match_label = Label(root, text="Start Match")
+start_match_label.grid(row=2, column=0)
+end_match_label = Label(root, text="End Match")
+end_match_label.grid(row=2, column=2)
+start_year_label = Label(root, text="Start Year")
+start_year_label.grid(row=0, column=0)
+end_year_label = Label(root, text="End Year")
+end_year_label.grid(row=0, column=2)
 select_Team = Label(root, text="Choose two Teams")
-select_Team.grid(row = 3, column = 1)
+select_Team.grid(row = 5, column = 1)
 home_Team = Label(root, text="Home")
-home_Team.grid(row = 3, column = 0)
+home_Team.grid(row = 5, column = 0)
 away_Team = Label(root, text="Guest")
-away_Team.grid(row = 3, column = 2)
+away_Team.grid(row = 5, column = 2)
+vstext = Label(root, text="VS")
+vstext.grid(row=6, column=1)
+result_text = Label(root, text="Expected results")
+result_text.grid(row=7, column=1)
+result_team1 = Label(root, text="not trained")
+result_team1.grid(row=7, column=0)
+result_team2 = Label(root, text="not trained")
+result_team2.grid(row=7, column=2)
 
 
 #set buttons
-crwalBtn = Button(root, text="Crawler", command=makeCrawlThread) # crawl
-crwalBtn.grid(row=0, column=1)
+NextOrResetBtn = Button(root, text="Next", command=NextOrReset) # reset
+NextOrResetBtn.grid(row=1, column=1)
+crawlBtn = Button(root, text="Crawl", command=makeCrawlThread, state=DISABLED) # crawl
+crawlBtn.grid(row=2, column=1)
 startTrainBtn = Button(root, text="Start Training", command=traning, state=DISABLED)
-startTrainBtn.grid(row=2, column=1)
-vstext = Label(root, text="VS")
-vstext.grid(row=4, column=1)
-result_text = Label(root, text="Expected results")
-result_text.grid(row=5, column=1)
-result_team1 = Label(root, text="not trained")
-result_team1.grid(row=5, column=0)
-result_team2 = Label(root, text="not trained")
-result_team2.grid(row=5, column=2)
+startTrainBtn.grid(row=4, column=1)
+
 
 # Create a Tkinter variable
 hometeamVar = StringVar(root) #home team
@@ -195,29 +245,44 @@ endMatch.set(str(34)) # set the default option
 
 homeTeamMenu = OptionMenu(root, hometeamVar, *team_list)
 geustTeamMenu = OptionMenu(root, guestteamVar, *team_list)
-homeTeamMenu.grid(row = 4, column =0)
-geustTeamMenu.grid(row = 4, column =2)
+homeTeamMenu.grid(row = 6, column =0)
+geustTeamMenu.grid(row = 6, column =2)
 
-startYearMenu = OptionMenu(root, startYear, *year_list)
-startYearMenu.grid(row = 0, column =0)
-endYearMenu = OptionMenu(root, endYear, *year_list)
-endYearMenu.grid(row = 0, column =2)
+startYearMenu = OptionMenu(root, startYear, *start_Year_list)
+startYearMenu.grid(row = 1, column =0)
+endYearMenu = OptionMenu(root, endYear, *end_Year_list)
+endYearMenu.grid(row = 1, column =2)
 
-startMatchMenu = OptionMenu(root, startMatch, *day_list)
-startMatchMenu.grid(row = 1, column =0)
-endMatchMenu = OptionMenu(root, endMatch, *day_list)
-endMatchMenu.grid(row = 1, column =2)
-
+startMatchMenu = OptionMenu(root, startMatch, *start_Match_list)
+startMatchMenu.grid(row = 3, column =0)
+endMatchMenu = OptionMenu(root, endMatch, *end_Match_list)
+endMatchMenu.grid(row = 3, column =2)
+endYearMenu.config(state="disabled")
+endMatchMenu.config(state="disabled")
 # on change dropdown value
 def change_dropdown1(*args):
     TeamMenuChange(hometeamVar.get(), guestteamVar.get())
 def change_dropdown2(*args):
     TeamMenuChange(hometeamVar.get(), guestteamVar.get())
+def setEndMatchList(*args):
+    global startYear
+    global startMatch
+    global endYear
+    global endMatch
+    global endMatchMenu
+    #removed all
+    endMatchMenu['menu'].delete(0, 'end')
+    if(startYear.get()==endYear.get()):
+        for x in range(1,35-int(startMatch.get())+1):
+            endMatchMenu['menu'].add_command(label=str(35-x), command=tkinter1._setit(endMatch, str(35-x)))    
+    else:
+        for x in range(1,35):
+            endMatchMenu['menu'].add_command(label=str(35-x), command=tkinter1._setit(endMatch, str(35-x))) 
 
 # link function to change dropdown
 hometeamVar.trace('w', change_dropdown1)
 guestteamVar.trace('w', change_dropdown2)
-
+endYear.trace('w', setEndMatchList)
 # menu for selecting an algorithm 
 OPTIONS = [
     "Mini Algorithm", # add new algorithm names here
@@ -226,7 +291,7 @@ algo = StringVar(root)
 algo.set(OPTIONS[0]) # set the default option
 algoSelMenu = OptionMenu(root, algo, *OPTIONS)
 algoSelMenu.grid(row = 3, column = 1)
-
+algoSelMenu.config(state="disabled")
 
 
 root.mainloop()
