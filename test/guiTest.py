9dff03ae0c951253ec05cb83af1aeecbbd31ffd4
Jung95 <jung95219@gmail.com>
2019-07-01 12:55:10 +0200

    Wrote DOTO for guiTest

---
+++ test/guiTest.py
@@ -0,0 +1,17 @@
+import unittest
+
+class GUITest(unittest.TestCase): 
+"""
+TODO:
+ selcet random startdate in the list and invoke next button and  selcet random enddate in the list
+ invoke crawl button, try changed algo and invoke traingstart. and change team1 and team2
+"""
+
+       
+
+
+
+
+#Run unittest
+if __name__ == '__main__':  
+    unittest.main()
