6850790fe8582372fa00ddc266e2797614506edf
Jung95 <jung95219@gmail.com>
2019-07-01 12:56:36 +0200

    changed commentary

--- test/guiTest.py
+++ test/guiTest.py
@@ -1,12 +1,11 @@
 import unittest
 
 class GUITest(unittest.TestCase): 
-"""
-TODO:
+'''
  selcet random startdate in the list and invoke next button and  selcet random enddate in the list
  invoke crawl button, try changed algo and invoke traingstart. and change team1 and team2
-"""
-
+'''
+    pass
        
 
 
